9f69fa8d3ddbe522bed442a4a8c53ad470cb9dbe
victorfernandesraton <vfbrato@gmail.com>
2022-09-06 02:14:27 -0300

    ussing image size

--- builder/pdf.go
+++ builder/pdf.go
@@ -2,6 +2,9 @@ package builder
 
 import (
 	"fmt"
+	"image"
+	"os"
+	"path/filepath"
 
 	"github.com/go-pdf/fpdf"
 )
@@ -10,11 +13,23 @@ func BuildToPdf(images []string, outPath string) error {
 	pdfPath := fmt.Sprintf("%s.pdf", outPath)
 	fmt.Printf("Generate pd %s\n", pdfPath)
 	pdf := fpdf.New("P", "mm", "A4", "")
-	for _, v := range images {
-		pdf.AddPage()
-		pdf.ImageOptions(v, 0, 0, 0, 0, false, fpdf.ImageOptions{
-			ReadDpi: true,
-		}, 0, "")
+	for _, imgFile := range images {
+
+		if reader, err := os.Open(filepath.Join(imgFile)); err == nil {
+			defer reader.Close()
+			im, _, err := image.DecodeConfig(reader)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: %v\n", imgFile, err)
+				continue
+			}
+			fmt.Printf("%s %d %d\n", imgFile, im.Width, im.Height)
+			pdf.AddPage()
+			pdf.ImageOptions(imgFile, 0, 0, 0, 0, false, fpdf.ImageOptions{
+				ReadDpi: true,
+			}, 0, "")
+		} else {
+			return err
+		}
 	}
 	return pdf.OutputFileAndClose(pdfPath)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,8 @@ module github.com/victorfernandesraton/hq-now-dowloader
 
 go 1.19
 
+require github.com/google/go-cmp v0.5.8
+
 require (
 	github.com/go-pdf/fpdf v0.6.0 // indirect
-	github.com/google/go-cmp v0.5.8
 )
