0f82f84bc6d18506b642c25684c7a590b8f212f1
victorfernandesraton <vfbrato@gmail.com>
2022-09-07 16:14:59 -0300

    parse from chapter

--- main.go
+++ main.go
@@ -22,7 +22,23 @@ func main() {
 			}
 		},
 	}
+
+	var cmdChapter = &cobra.Command{
+		Use:   "ch [url from hq chapter]",
+		Short: "dowload hq chapter",
+		Long:  "Dowload hq chapter and generate pdf with use a valid url",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			info, err := convert.ParseUrlFromChapter(args[0])
+			if err != nil {
+				panic(err)
+			}
+			if err = commands.CreateByChapter(info.ID); err != nil {
+				panic(err)
+			}
+		},
+	}
 	var rootCmd = &cobra.Command{Use: "app"}
-	rootCmd.AddCommand(cmdHq)
+	rootCmd.AddCommand(cmdHq, cmdChapter)
 	rootCmd.Execute()
 }
